1abe79df15947938b5091269e68019408ad06d45
Félix Paradis <felixparadis@gmail.com>
2023-04-27 10:40:20 -0400

    Update querySelector for Le Soleil

--- scrapers/leSoleil.js
+++ scrapers/leSoleil.js
@@ -7,7 +7,7 @@ export async function getSoleilImg() {
   try {
     const response = await got(url)
     const doc = new JSDOM(response.body).window.document
-    const img = doc.querySelector("._full-article-cartoon_145kag img").src
+    const img = doc.querySelector(".promo-image img").src
     
     return `<h2>Le Soleil</h2>
     <br>
