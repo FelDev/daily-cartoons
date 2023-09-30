b8985729701bf07424ee11b6f45bf935cc649d83
Felix Paradis <felixparadis@gmail.com>
2023-09-30 12:04:11 -0400

    fix Journal de Mtl image

--- scrapers/journalDeMontreal.js
+++ scrapers/journalDeMontreal.js
@@ -9,16 +9,16 @@ export async function getJdmImg() {
     const doc = new JSDOM(response.body).window.document
     const srcset = doc.querySelector("#landingSection .cartoon-item img").srcset
     let imageUrlString = srcset.split(',')[0].trim();
-
-    let imageURL = new URL(imageUrlString);
-    let searchParams = new URLSearchParams(imageURL.search);
-    searchParams.set('width', '1000');
-    imageURL.search = searchParams.toString();
-    let newImageUrl = imageURL.toString();
+    let cleanImageUrl = `${imageUrlString.split("?")[0]}?impolicy=resize&width=1000` // remove query params, replace with simple resize
+    // let imageURL = new URL(imageUrlString);
+    // let searchParams = new URLSearchParams(imageURL.search);
+    // searchParams.set('width', '1000');
+    // imageURL.search = searchParams.toString();
+    // let newImageUrl = imageURL.toString();
 
     return `<h2>Journal de Montréal</h2>
     <br>
-    <img src="${newImageUrl}">`
+    <img src="${cleanImageUrl}">`
     
   } catch (err) {
     console.log('@err: ', err)
