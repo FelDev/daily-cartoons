602ca273a94f9faa03d144607464a5459a93b88e
Felix Paradis <felixparadis@gmail.com>
2023-10-08 11:58:16 -0400

    fix le Soleil hurr dhurr undefined variables no good huhuhu

--- scrapers/leSoleil.js
+++ scrapers/leSoleil.js
@@ -2,10 +2,11 @@ import got from 'got'
 import jsdom from 'jsdom'
 const { JSDOM } = jsdom
 const baseURL = "https://www.lesoleil.com"
+const baseCartoonURL =`${baseURL}/opinions/caricatures`
 
 export async function getSoleilImg() {
   try {
-    const resPage1 = await got(`${baseURL}/opinions/caricatures`)
+    const resPage1 = await got(baseCartoonURL)
     const doc1 = new JSDOM(resPage1.body).window.document
     const cartoonUrl = doc1.querySelector("div.promo-image > a").href
     const resPage2 = await got(`${baseURL}${cartoonUrl}`)
@@ -13,7 +14,7 @@ export async function getSoleilImg() {
     const img = doc2.querySelector(".lead-art-wrapper img").src
     
     return `
-    <a href="${url}">
+    <a href="${baseCartoonURL}">
       <h2>Le Soleil</h2>
     </a>
     <br>
