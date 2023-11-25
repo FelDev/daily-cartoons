149ff60f993cfb541da90a816443466904b12aac
Felix Paradis <felixparadis@gmail.com>
2023-11-25 10:13:34 -0500

    fix JDM AGAIN

--- scrapers/journalDeMontreal.js
+++ scrapers/journalDeMontreal.js
@@ -2,21 +2,19 @@ import got from 'got'
 import jsdom from 'jsdom'
 const { JSDOM } = jsdom
 const url = "https://www.journaldemontreal.com/opinions/caricatures"
-let cartoonURL
 
 export async function getJdmImg() {
   try {
     const resPage1 = await got(url)
     const doc1 = new JSDOM(resPage1.body).window.document
-    cartoonURL = doc1.querySelector(".cartoon-img > a").href
-
-    const srcset = doc1.querySelector(".cartoon-img img").srcset
+    const srcset = doc1.querySelector(".item-inner > a img").srcset
+    
     let imageUrlString = srcset.split(',')[1].trim();
     
     let cleanImageUrl = `${imageUrlString.split("?")[0]}?impolicy=resize&width=1000` // remove query params, replace with simple resize
-
+    
     return `
-    <a href="${cartoonURL}">
+    <a href="${url}">
       <h2>Journal de Montréal</h2>
     </a>
     <br>
@@ -26,6 +24,6 @@ export async function getJdmImg() {
   } catch (err) {
     console.log('@err: ', err)
     
-    return `<p>Le Journal de Montréal ça chié...<a href="${cartoonURL}"> </a></p>`
+    return `<p>Le Journal de Montréal ça chié...<a href="${url}"> </a></p>`
   }
 }
